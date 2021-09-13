5440325caa2a3e46d050d12d32a7b15ee83bdcb9
Rhyine Stewart <rhyinestewart@gmail.com>
2021-09-13 15:23:35 -0500

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import '../scss/style.scss';
+// import 'bootstrap';
+
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Starter', 'Text'], ' ');
+    element.classList.add('hello');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
