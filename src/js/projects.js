eaf988f036504ef968ed9275b308f954073a2cc0
Rhyine <rhyinestewart@gmail.com>
2021-09-16 18:34:59 -0500

    Setup projects section data and logic

---
+++ src/js/projects.js
@@ -0,0 +1,9 @@
+class Projects {
+    constructor(data, template, templateContainer) {
+        this.data = data;
+        this.template = template;
+        this.templateContainer = templateContainer;
+    }
+
+
+}
